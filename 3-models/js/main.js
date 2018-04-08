8b41e9598bae16d3f75010cb947ab01675c07a49
David Sabalete <dsabalete@gmail.com>
2018-04-08 21:46:08 +0200

    Inheritance in backbone

--- 3-models/js/main.js
+++ 3-models/js/main.js
@@ -1,35 +1,58 @@
 var Song = Backbone.Model.extend({
     initialize: function () {
-        console.log('A new song has been created.');
+        console.log('A new song has been created.')
     },
     defaults: {
         genre: 'Jazz'
     },
     validate: function(attrs) {
         if(!attrs.title) {
-            return "Title is required";
+            return "Title is required"
         }
     }
-});
+})
 
 var song = new Song({
     title: 'Blue in Green',
     artist: 'Miles Davis',
     publishYear: 1959
-});
+})
 
 // set a property or change an existing one
-song.set('title', 'Blue in Greeen');
+song.set('title', 'Blue in Greeen')
 
 // get property value
-var title = song.get('title');
+var title = song.get('title')
 
 // exist the property?
-var hasTitle = song.has('title');
+var hasTitle = song.has('title')
 
 // remove property
-song.unset('title');
+song.unset('title')
 
 // remove all property from model
-song.clear();
+song.clear()
 
+// Return if object fullfil validation rules
+song.isValid()
+
+// literal error in case of a validation Error
+var lastError = song.validationError
+
+var Animal = Backbone.Model.extend({
+    walk: function() {
+        console.log("Animal walking...")
+    }
+})
+
+var Dog = Animal.extend({
+    walk: function() {
+        Animal.prototype.walk.applly(this)
+
+        console.log("Dog walking...");
+    }
+})
+
+var dog = new Dog()
+
+dog.walk()
