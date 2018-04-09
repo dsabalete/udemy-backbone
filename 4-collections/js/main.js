c1ac731fe252a7b5a7e245d7cb61130a9f3cccbe
David Sabalete <dsabalete@gmail.com>
2018-04-09 23:53:53 +0200

    Creating collections

--- 4-collections/js/main.js
+++ 4-collections/js/main.js
@@ -1,4 +1,19 @@
+var Song = Backbone.Model.extend()
 
-// In the first few sections, we do all the coding here.
-// Later, you'll see how to organize your code into separate
-// files and modules.
+var Songs = Backbone.Collection.extend({
+    model: Song
+})
+
+var songs = new Songs([
+    new Song({ title: 'Song 1'}),
+    new Song({ title: 'Song 2'}),
+    new Song({ title: 'Song 3'})
+])
+
+songs.add(new Song({ title: 'Song 4'}))
+
+var firstSong = songs.at(0)
+
+var songWithIdC1 = songs.get('c1')
+
+songs.remove(firstSong)
