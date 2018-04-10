8feeda832e7e1922914a79265248a3211f35cb5e
David Sabalete <dsabalete@gmail.com>
2018-04-10 23:25:13 +0200

    My project for 4-collections

--- 4-collections_project/js/main.js
+++ 4-collections_project/js/main.js
@@ -1,4 +1,33 @@
+var Vehicle = Backbone.Model.extend({
+    idAttribute: 'registrationNumber',
+    urlRoot: '/api/vehicles',
+    validate: function(attrs) {
+        if (!attrs.registrationNumber == null || attrs.registrationNumber == undefined) {
+            return 'Registration number is required.'
+        }
+    },
+    start: function() {
+        console.log('Vehicle started')
+    }
+})
 
-// In the first few sections, we do all the coding here.
-// Later, you'll see how to organize your code into separate
-// files and modules.
+var Vehicles = Backbone.Collection.extend({
+    model: Vehicle
+})
+
+var cars = new Vehicles([
+    new Vehicle({ registrationNumber: 'XLI887', color: 'Blue'}),
+    new Vehicle({ registrationNumber: 'ZNP123', color: 'Blue'}),
+    new Vehicle({ registrationNumber: 'XUV456', color: 'Gray'})
+])
+
+var blueCars = cars.where({ color: 'Blue' })
+blueCars.forEach((car) => console.log(car))
+
+var car2delete = cars.findWhere({ registrationNumber: 'XLI887' })
+console.log(car2delete)
+
+cars.remove(car2delete)
+console.log(cars.toJSON())
+
+cars.forEach((car) => console.log(car))
