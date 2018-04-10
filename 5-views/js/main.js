cc2a490702bd8acc90a8ff13708fc0c713c42cbf
David Sabalete <dsabalete@gmail.com>
2018-04-10 23:57:50 +0200

    Passingdata to views

--- 5-views/js/main.js
+++ 5-views/js/main.js
@@ -1,40 +1,35 @@
+var Song = Backbone.Model.extend()
+
+var Songs = Backbone.Collection.extend({
+    model: Song
+})
+
 var SongView = Backbone.View.extend({
+    tagName: 'li',
+
     render: function () {
-        this.$el.html('Hello World')
+        this.$el.html(this.model.get('title'))
 
         return this
     }
 })
 
-var songView = new SongView({ el: '#container' })
-songView.render()
+var SongsView = Backbone.View.extend({
+    render: function() {
+        var self = this
 
-// ---
-
-// if you don't especify the element:
-var songView = new SongView()
-
-$('#container').html(songView.render().$el)
-
-// ---
-
-// you can customize the view
-var CustomizedSongView = Backbone.View.extend({
-    tagName: 'span',
-
-    className: 'song',
-
-    id: '1234',
-
-    attributes: {
-        'data-genre': 'Jazz'
-    },
-
-    render: function () {
-        this.$el.html('Hello World')
-
-        return this
+        this.model.forEach(function(song){
+            var songView = new SongView({ model: song })
+            self.$el.append(songView.render().$el)
+        })
     }
 })
+var songs = new Songs([
+    new Song({ title: 'Blue in Green' }),
+    new Song({ title: 'So What' }),
+    new Song({ title: 'All Blues' })
+])
 
+var songsView = new SongsView({ el: '#songs', model: songs })
+songsView.render()
 
