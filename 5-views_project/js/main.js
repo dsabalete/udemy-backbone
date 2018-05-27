518880797522029d97ab30124ee423d7b1fde0ba
David Sabalete <dsabalete@gmail.com>
2018-05-27 20:51:11 +0200

    View project and solution

--- 5-views_project/js/main.js
+++ 5-views_project/js/main.js
@@ -1,82 +1,68 @@
-		
-// In the first few sections, we do all the coding here.
-// Later, you'll see how to organize your code into separate
-// files and modules.
-
 var Vehicle = Backbone.Model.extend({
-
-	idAttribute: "registrationNumber",
-
-	urlRoot: "/api/vehicles",
-
-	validate: function(attrs){
-		if (!attrs.registrationNumber)
-			return "Vehicle is not valid.";
+	idAttribute: 'registrationNumber',
+	
+	urlRoot: '/api/vehicles',
+	
+    validate: function(attrs) {
+        if (!attrs.registrationNumber == null || attrs.registrationNumber == undefined) {
+            return 'Registration number is required.'
+        }
 	},
-
-	start: function(){
-		console.log("Vehicle started.");
-	}
-});
+	
+    start: function() {
+        console.log('Vehicle started')
+    }
+})
 
 var Vehicles = Backbone.Collection.extend({
-	Model: Vehicle
-});
-
-var Car = Vehicle.extend({
-	start: function(){
-		console.log("Car with registration number " + this.get("registrationNumber") + " started.");
-	}
-});
+    model: Vehicle
+})
 
 var VehicleView = Backbone.View.extend({
-	tagName: "li",
-
-	className: "vehicle",
-
+	tagName: 'li',
+	
+	className: 'vehicle',
+	
+	attributes: {
+		"data-color": "white"
+	},
+	
 	events: {
-		"click .delete": "onDelete",
+		"click .delete": "onDelete"
+	},
+
+	onDelete: function(e) {
+		this.remove()
 	},
 
 	render: function() {
-		var source = $("#vehicleTemplate").html();
-		var template = _.template(source);
+		var source = $("#vehicleTemplate").html()
+		var template = _.template(source)
+		
+		this.$el.html(template(this.model.toJSON()))
+		this.$el.attr("data-color", this.model.get("color"))
 
-		this.$el.html(template(this.model.toJSON()));
-		this.$el.attr("data-color", this.model.get("color"));
-
-		return this;
-	},
-
-	onDelete: function(){
-		this.remove();
+		return this
 	}
-});
+})
 
 var VehiclesView = Backbone.View.extend({
-	tagName: "ul",
+	tagName: 'ul',
 
-	render: function(){
-		this.collection.each(function(vehicle){
-			var vehicleView = new VehicleView({ model: vehicle });
-			this.$el.append(vehicleView.render().$el);
-		}, this); // note the reference to this here. When you set
-		// the "this" pointer here (as the second argument to the 
-	    // each method, you'll be able to access "this" inside the 
-	    // callback function in the each method:
-	    //
-	    // this.$el.append(...)
-
-		return this;
+	render: function() {
+		this.collection.each(function(vehicle) {
+			var vehicleView = new VehicleView({ model: vehicle })
+			this.$el.append(vehicleView.render().$el)
+		}, this)
+		return this
 	}
-});
+})
 
 var vehicles = new Vehicles([
-	new Car({ registrationNumber: "XLI887", color: "Blue" }),
-	new Car({ registrationNumber: "ZNP123", color: "Blue" }),
-	new Car({ registrationNumber: "XUV456", color: "Gray" })
-]);
+	new Vehicle({ registrationNumber: 'XLI887', color: 'Blue'}),
+	new Vehicle({ registrationNumber: 'ZNP123', color: 'Blue'}),
+	new Vehicle({ registrationNumber: 'XUV456', color: 'Gray'})
+])
 
-var vehiclesView = new VehiclesView({ collection: vehicles });
-$("#container").html(vehiclesView.render().$el);
-
+var vehiclesView = new VehiclesView({ collection: vehicles })
+$("#container").html(vehiclesView.render().$el)
