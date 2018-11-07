a8874828fa267fde4ea1930f7c0a450b8f3293f5
David Sabalete <dsabalete@gmail.com>
2018-11-07 20:25:27 +0100

    Binding and Triggering custom events

--- 6-events/js/main.js
+++ 6-events/js/main.js
@@ -1,3 +1,27 @@
 var person = {
-    name: "David"
+    name: "David",
+
+    walk: function() {
+        this.trigger("walking", {
+            speed: 1,
+            startTime: "08:00"
+        });
+    }
 }
+
+_.extend(person, Backbone.Events);
+
+// person.on("walking", function(e) {
+//     console.log("Person is walking");
+//     console.log("Event Args", e);
+// });
+
+// person.off("walking");
+
+person.once("walking", function(e) {
+    console.log("Person is walking");
+    console.log("Event Args", e);
+});
+
+person.walk();
+person.walk();
