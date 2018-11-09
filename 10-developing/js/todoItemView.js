c9812556e5b91c15f5106dd38913dc3361d8fdbd
David Sabalete <dsabalete@gmail.com>
2018-11-09 13:02:07 +0100

    templating

--- 10-developing/js/todoItemView.js
+++ 10-developing/js/todoItemView.js
@@ -26,10 +26,12 @@ var TodoItemView = Backbone.View.extend({
 
     render: function () {
         this.$el.attr("id", this.model.id);
+
         this.$el.toggleClass("completed", this.model.get("completed"));
 
-        var checked = this.model.get("completed") ? "checked" : "";
-        this.$el.html("<input id='toggle' type='checkbox'" + checked + "></input>" + this.model.escape("title") + "<button id='delete'>Delete</button>");
+        var template = $("#todoItemTemplate").html();
+        var html = Mustache.render(template, this.model.toJSON());
+        this.$el.html(html);
 
         return this;
     }
